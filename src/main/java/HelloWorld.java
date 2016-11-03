c4eef326582d37b6a17feed0f20c090d6f50b0d2
Zimeon <severalstars@gmail.com>
2016-11-03 15:32:12 +0100

    Lade till en knapp

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -8,7 +8,17 @@ public class HelloWorld {
    public HelloWorld() {
       System.out.println("HelloWorld started!");
    }
-	
+
+   String userName="Nanashi";
+
+   public String getUserName() {
+       return userName;
+   }
+
+   public void setUserName(String userName) {
+      this.userName = userName;
+   }
+
    public String getMessage() {
       return "Hello World!";
    }
