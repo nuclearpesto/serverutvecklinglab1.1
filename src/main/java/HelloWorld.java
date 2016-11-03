3f8cd0e6ab6bcc4fd12ada36710397c494182fb5
David <david.boeryd@gmail.com>
2016-11-03 14:45:28 +0100

    gitignore

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,9 @@
 
-
-package com.tutorialspoint.test;
-
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 @ManagedBean(name = "helloWorld", eager = true)
+@SessionScoped
 public class HelloWorld {
    public HelloWorld() {
       System.out.println("HelloWorld started!");
