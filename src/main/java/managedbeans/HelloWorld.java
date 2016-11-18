b59cb81ac9212221c4d566691cdab42896b30392
David <david.boeryd@gmail.com>
2016-11-18 18:41:34 +0100

    added new handler class, removed som unnedded test classes

---
+++ src/main/java/managedbeans/HelloWorld.java
@@ -0,0 +1,40 @@
+package managedbeans;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "helloWorld", eager = true)
+@SessionScoped
+public class HelloWorld {
+    public HelloWorld() {
+        System.out.println("managedbeans.HelloWorld started! " + System.getProperty("user.dir"));
+    }
+
+    private String userName = "Nanashi";
+    private String student = "nuffeli";
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String replyHello() {
+        return "Hello" + userName;
+    }
+
+    public String getMessage() {
+        return "Hello World!";
+    }
+
+    public void setStudent(String s) {
+        student = s;
+    }
+
+    public String getStudent() {
+      return "GO AWAY!";
+    }
+}
